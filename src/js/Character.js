4334f013e305f9854d30900400330566608d7923
Anna <anna4776497@gmail.com>
2022-04-15 19:44:13 +0300

    Без ошибок

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,37 +1,34 @@
 export default class Character {
   constructor(name, type) {
-    let list = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie']
-  if (name.length <2 || name.length > 10 || name === undefined){
+    const list = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
+    if (name.length < 2 || name.length > 10 || name === undefined) {
       throw new Error('Длина имени не соответствует условиям');
-  }
-  if (!list.includes(type)) {
+    }
+    if (!list.includes(type)) {
       throw new Error('Не соответствие заданным типам');
-  }
-  this.name = name;
-  this.type = type;
- };
-  health = 100;
-  level =  1;
-  attack = 0;
-  defence = 0;
-
-  levelUp(){
-    if(this.health === 0){
-      throw new Error('Нельзя повысить левел умершего')
     }
-    this.level = this.level + 1;
-    this.attack = this.attack * 1.2;
-    this.defence = this.defence * 1.2;
+    this.name = name;
+    this.type = type;
     this.health = 100;
-  };
+    this.level = 1;
+    this.attack = 0;
+    this.defence = 0;
+  }
 
-  damage(points){
-    if(this.health === 0){
-      throw new Error('Нельзя выполнить для умершего')
+  levelUp() {
+    if (this.health === 0) {
+      throw new Error('Нельзя повысить левел умершего');
     }
-    this.health =this.health - points * (1 - this.defence / 100)
+    this.level += 1;
+    this.attack *= 1.2;
+    this.defence *= 1.2;
+    this.health = 100;
+  }
+
+  damage(points) {
+    if (this.health === 0) {
+      throw new Error('Нельзя выполнить для умершего');
+    }
+    this.health -= points * (1 - this.defence / 100);
   }
 }
-  
-  
-  
